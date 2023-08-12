6ae59eef2dadedab2f048283828b0a5aab9b54a9
Ben Heidemann <ben@heidemann.co.uk>
2023-08-12 01:08:02 +0100

    feat: emit types

--- main.ts
+++ main.ts
@@ -236,6 +236,278 @@ export function collapseUnknownTypes(_types: UnknownType[]): UnknownType {
   return { kind: "unknown" };
 }
 
+type EmitFn = (data: string) => void;
+type GetTypeNameFn = () => string;
+
+export function emitType(
+  name: string,
+  type: Type,
+  getTypeName: GetTypeNameFn,
+  emit: EmitFn,
+  preferInline: boolean,
+): string | void {
+  switch (type.kind) {
+    case "struct":
+      return emitStructType(
+        name,
+        type,
+        getTypeName,
+        emit,
+        preferInline,
+      );
+    case "enum":
+      return emitEnumType(
+        name,
+        type,
+        getTypeName,
+        emit,
+        preferInline,
+      );
+    case "map":
+      return emitMapType(
+        name,
+        type,
+        getTypeName,
+        emit,
+        preferInline,
+      );
+    case "option":
+      return emitOptionType(
+        name,
+        type,
+        getTypeName,
+        emit,
+        preferInline,
+      );
+    case "array":
+      return emitArrayType(
+        name,
+        type,
+        getTypeName,
+        emit,
+        preferInline,
+      );
+    case "primitive":
+      return emitPrimitiveType(
+        name,
+        type,
+        getTypeName,
+        emit,
+        preferInline,
+      );
+    case "null":
+      return emitNullType(
+        name,
+        type,
+        getTypeName,
+        emit,
+        preferInline,
+      );
+    case "unknown":
+      return emitUnknownType(
+        name,
+        type,
+        getTypeName,
+        emit,
+        preferInline,
+      );
+  }
+}
+
+export function emitStructType(
+  name: string,
+  type: StructType,
+  getTypeName: GetTypeNameFn,
+  emit: EmitFn,
+  _preferInline: boolean,
+) {
+  const fields = Array
+    .from(type.fields.entries())
+    .sort(([key1], [key2]) => key1.localeCompare(key2))
+    .map(([key, value]) => {
+      const typeName = getTypeName();
+      const inlineType = emitType(typeName, value, getTypeName, emit, true);
+      return `  ${key}: ${inlineType ?? typeName},`;
+    });
+  emit(
+    [
+      `pub struct ${name} {`,
+      ...fields,
+      "}",
+      "",
+    ].join("\n")
+  );
+}
+
+export function emitEnumType(
+  name: string,
+  type: EnumType,
+  getTypeName: GetTypeNameFn,
+  emit: EmitFn,
+  _preferInline: boolean,
+) {
+  const variants = Array
+    .from(type.variants.entries())
+    .sort(([key1], [key2]) => key1.localeCompare(key2))
+    .map(([key, value]) => {
+      const typeName = getTypeName();
+      const inlineType = emitType(typeName, value, getTypeName, emit, true);
+      return `  ${key}(${inlineType ?? typeName}),`;
+    });
+  emit(
+    [
+      `pub enum ${name} {`,
+      ...variants,
+      "}",
+      "",
+    ].join("\n")
+  );
+}
+
+export function emitMapType(
+  name: string,
+  type: MapType,
+  getTypeName: GetTypeNameFn,
+  emit: EmitFn,
+  preferInline: boolean,
+) {
+  const typeName = getTypeName();
+  const innerInlineType = emitType(typeName, type.valueType, getTypeName, emit, true);
+  const inlineType = `std::collections::HashMap<String, ${innerInlineType ?? typeName}>`;
+  if (preferInline) {
+    return inlineType;
+  }
+  emit(
+    [
+      `pub type ${name} = ${inlineType};`,
+      "",
+    ].join("\n")
+  );
+}
+
+export function emitOptionType(
+  name: string,
+  type: OptionType,
+  getTypeName: GetTypeNameFn,
+  emit: EmitFn,
+  preferInline: boolean,
+) {
+  const typeName = getTypeName();
+  const innerInlineType = emitType(typeName, type.valueType, getTypeName, emit, true);
+  const inlineType = `Option<${innerInlineType ?? typeName}>`;
+  if (preferInline) {
+    return inlineType;
+  }
+  emit(
+    [
+      `pub type ${name} = ${inlineType};`,
+      "",
+    ].join("\n")
+  );
+}
+
+export function emitArrayType(
+  name: string,
+  type: ArrayType,
+  getTypeName: GetTypeNameFn,
+  emit: EmitFn,
+  preferInline: boolean,
+) {
+  const typeName = getTypeName();
+  const innerInlineType = emitType(typeName, type.elementType, getTypeName, emit, true);
+  const inlineType = `Vec<${innerInlineType ?? typeName}>`;
+  if (preferInline) {
+    return inlineType;
+  }
+  emit(
+    [
+      `pub type ${name} = ${inlineType};`,
+      "",
+    ].join("\n")
+  );
+}
+
+export function emitPrimitiveType(
+  name: string,
+  type: PrimitiveType,
+  _getTypeName: GetTypeNameFn,
+  emit: EmitFn,
+  preferInline: boolean,
+) {
+  const inlineType = getPrimitiveName(type.name);
+  if (preferInline) {
+    return inlineType;
+  }
+  emit(
+    [
+      `pub type ${name} = ${inlineType};`,
+      "",
+    ].join("\n")
+  );
+}
+
+export function emitNullType(
+  name: string,
+  _type: NullType,
+  _getTypeName: GetTypeNameFn,
+  emit: EmitFn,
+  preferInline: boolean,
+) {
+  const inlineType = "()";
+  if (preferInline) {
+    return inlineType;
+  }
+  emit(
+    [
+      `pub type ${name} = ${inlineType};`,
+      "",
+    ].join("\n")
+  );
+}
+
+export function emitUnknownType(
+  name: string,
+  _type: UnknownType,
+  _getTypeName: GetTypeNameFn,
+  emit: EmitFn,
+  preferInline: boolean,
+) {
+  const inlineType = "Unknown";
+  if (preferInline) {
+    return inlineType;
+  }
+  emit(
+    [
+      `pub type ${name} = ${inlineType};`,
+      "",
+    ].join("\n")
+  );
+}
+
+export function emitTypes(type: Type) {
+  let code = "";
+  const emit = (data: string) => {
+    code += data;
+  }
+  let counter = 0;
+  const getTypeName = () => {
+    return `T${counter++}`;
+  };
+  emitType("Root", type, getTypeName, emit, false);
+  return code;
+}
+
+export function getPrimitiveName(primitive: PrimitiveType["name"]) {
+  switch (primitive) {
+    case "string":
+      return "String";
+    case "number":
+      return "f64";
+    case "boolean":
+      return "bool";
+  }
+}
+
 if (import.meta.main) {
   console.log('Ok');
 }
--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@ import {
   it,
 } from "https://deno.land/std@0.198.0/testing/bdd.ts";
 import { assertSnapshot } from "https://deno.land/std@0.198.0/testing/snapshot.ts";
-import { parseJson } from "./main.ts";
+import { emitTypes, parseJson } from "./main.ts";
 import { Json } from "./types.ts";
 
 describe("parseJson", () => {
@@ -101,3 +101,101 @@ describe("parseJson", () => {
     await run(ctx, "object-8.json");
   });
 });
+
+describe("emitTypes", () => {
+  async function run(
+    ctx: Deno.TestContext,
+    filename: string,
+  ) {
+    const json = await Deno.readTextFile(`./fixtures/${filename}`);
+    const obj = JSON.parse(json) as Json;
+    const type = parseJson(obj);
+    const code = emitTypes(type);
+    await assertSnapshot(ctx, code);
+  }
+
+  it("emit array-0.json", async (ctx) => {
+    await run(ctx, "array-0.json");
+  });
+
+  it("emit array-1.json", async (ctx) => {
+    await run(ctx, "array-1.json");
+  });
+
+  it("emit array-2.json", async (ctx) => {
+    await run(ctx, "array-2.json");
+  });
+
+  it("emit boolean-false.json", async (ctx) => {
+    await run(ctx, "boolean-false.json");
+  });
+
+  it("emit boolean-true.json", async (ctx) => {
+    await run(ctx, "boolean-true.json");
+  });
+
+  it("emit map-0.json", async (ctx) => {
+    await run(ctx, "map-0.json");
+  });
+
+  it("emit map-1.json", async (ctx) => {
+    await run(ctx, "map-1.json");
+  });
+
+  it("emit map-2.json", async (ctx) => {
+    await run(ctx, "map-2.json");
+  });
+
+  it("emit null.json", async (ctx) => {
+    await run(ctx, "null.json");
+  });
+
+  it("emit number-negative.json", async (ctx) => {
+    await run(ctx, "number-negative.json");
+  });
+
+  it("emit number-positive.json", async (ctx) => {
+    await run(ctx, "number-positive.json");
+  });
+
+  it("emit number-zero.json", async (ctx) => {
+    await run(ctx, "number-zero.json");
+  });
+
+  it("emit object-0.json", async (ctx) => {
+    await run(ctx, "object-0.json");
+  });
+
+  it("emit object-1.json", async (ctx) => {
+    await run(ctx, "object-1.json");
+  });
+
+  it("emit object-2.json", async (ctx) => {
+    await run(ctx, "object-2.json");
+  });
+
+  it("emit object-3.json", async (ctx) => {
+    await run(ctx, "object-3.json");
+  });
+
+  it("emit object-4.json", async (ctx) => {
+    await run(ctx, "object-4.json");
+  });
+
+  it("emit object-5.json", async (ctx) => {
+    await run(ctx, "object-5.json");
+  });
+
+  it("emit object-6.json", async (ctx) => {
+    await run(ctx, "object-6.json");
+  });
+
+  it("emit object-7.json", async (ctx) => {
+    await run(ctx, "object-7.json");
+  });
+
+  it("emit object-8.json", async (ctx) => {
+    await run(ctx, "object-8.json");
+  });
+});
+
